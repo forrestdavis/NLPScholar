3247357ca785ba9eb8f057b93b354ea592eeb6d0
forrestdavis <fdavis@colgate.edu>
2024-08-29 16:52:49 -0400

    Updating environment

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ sys.path.append(str(Path(__file__).parent.parent))
 from src.utils.load_evaluations import load_evaluation
 from src.utils.load_trainers import load_trainer
 
+
 if len(sys.argv) > 1:
     configfname = sys.argv[1]
 else:
     configfname = 'config.yaml'
+
+sys.stderr.write(f'Reading from {configfname}\n')
 
 with open(configfname, 'r') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
